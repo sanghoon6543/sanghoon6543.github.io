6cfd5d1277f7ca6e731479a7901ad20c5be0bd40
shk <sanghoon6543@gmail.com>
2026-03-21 08:31:52 +0900

    update site

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,17 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>Hello</p>
+      <p> My research interest is obtaining information from the electromagnetic-matter interaction.
+          I consistently explored nanomaterials interacting with electromagnetic waves beyond the visible,
+          including physics calculation, optoelectronic device modelling, device evaluation, and
+          constructing optical instruments to characterize the device. </p>
+      <p> Humans perceive the intensity of wavelength from 400nm to 700nm as visible image,
+          and this visual information significantly influences recognition both on biological and intelligent systems.
+          Based on such stimuli, they react and predict future states. Although the visible provides abundant data,
+          electromagnetic waves possess many other parameters, and these would be valuable as X-Ray did. Based on the
+          understanding physical phenomena of electromagnetics, I aspire to pave and interpret novel forms of beneficial
+          information beyond our eyes. </p>
+
     </div>
   );
 }
