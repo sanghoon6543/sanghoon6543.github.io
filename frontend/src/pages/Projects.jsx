bdc6121afa9f0f6a88e94f347a9b5b85d530ec16
shk <sanghoon6543@gmail.com>
2026-03-21 09:11:38 +0900

    update site

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -4,8 +4,15 @@ import React from "react";
 export default function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
-      <p>Projects List</p>
+      <h1> Projects </h1>
+      <p> <a href="/files/Kim, Sang Hoon (Research Experience Summary).pdf" target="_blank" rel="noreferrer">
+           Overview (PDF) </a> </p>
+      <p> [6] SWIR Bio Imaging System (2026 ~ Present) </p>
+      <p> [5] SWIR Image Sensors based on Quantum Dot (2024 ~ Present) </p>
+      <p> [4] Direct X-Ray Detectors based on Quantum Dot (2023 ~ 2025) </p>
+      <p> [3] Metasurface for High Spatial Resolution indirect X-Ray Detectors (2023 ~ Present) </p>
+      <p> [2] Surface Enhanced Raman Scattering for Point of Care Testing (2021 ~ 2023) </p>
+      <p> [1] Graphene based Optoelectronic Device (2016 ~ 2018) </p>
     </div>
   );
 }
